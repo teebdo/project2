d7c4473c1471bba8fb01d20c0ad0e66637167110
teebdo <tlave11@lsu.edu>
2025-02-27 10:26:22 -0600

    push

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.getElementById('offcanvas-menu');
+
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded',!expanded);
+  menu.classList.toggle('expanded');
+});
